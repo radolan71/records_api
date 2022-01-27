688692d74491004af26cd3def74c78dc219a633d
Ed <egarciahernandez@albelli.net>
2022-01-27 21:39:49 +0100

    feat - response helpers

---
+++ src/common/responseHelper.ts
@@ -0,0 +1,38 @@
+import { Response } from 'express';
+import { ValidationError } from 'express-validator';
+import { Record } from '../app/record/RecordEntity';
+
+export interface BaseResponse {
+  code: number;
+  msg: string;
+  records: Record[];
+  metadata: Partial<ResponseMetadata>;
+}
+
+export type ErrorResponse = BaseResponse & {
+  errors: ValidationError[];
+};
+
+export interface ResponseMetadata {
+  page: number;
+  per_page: number;
+  page_count: number;
+  total_count: number;
+}
+
+export const createResponse = (
+  res: Response,
+  httpCode: number = 200,
+  appCode: number = 0,
+  message: string = '',
+  records: Record[] = [],
+  errors: ValidationError[] = [],
+  metadata: Partial<ResponseMetadata> = {},
+): void => {
+  const responseBody: Partial<ErrorResponse> = { code: appCode, msg: message, records: records, metadata: metadata };
+  if (errors.length) {
+    responseBody.errors = errors;
+  }
+
+  res.status(httpCode).send(responseBody);
+};
