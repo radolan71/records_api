f12cdb28042bfcbad9a17b8103aeff29125df8af
Ed <egarciahernandez@albelli.net>
2022-01-27 21:40:47 +0100

    format

--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,14 @@ import recordRoutes from './app/record/RecordRoutes';
 
 const router = express.Router({ strict: true });
 const baseRoutes = express.Router();
-
-router.use('/records', recordRoutes);
 baseRoutes.get('/', function (req, res) {
   res.send({
     name: `Getir Challenge ${process.env.BASE_API_URI}`,
     version: process.env.npm_package_version,
   });
 });
+
+router.use('/records', recordRoutes);
 router.use('/', baseRoutes);
 
 export default router;
