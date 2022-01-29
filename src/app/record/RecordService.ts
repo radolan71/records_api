b39b14f4cf0e36e5c042c28d32d5b1e9cb055ffe
Ed <egarciahernandez@albelli.net>
2022-01-29 14:37:09 +0100

    feat - improve query

--- src/app/record/RecordService.ts
+++ src/app/record/RecordService.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
 import { Application } from '../../Application';
 import { Record } from './RecordEntity';
+import { RecordSearchRequest } from './RecordMiddleware';
 
 export class RecordService {
   private static instance: RecordService;
@@ -13,55 +14,62 @@ export class RecordService {
   }
 
   getByConditions(req: Request): Promise<any> {
+    const body: RecordSearchRequest = req.body;
     return new Promise(async (resolve, reject) => {
       const dateQuery = {
-        $and: [{ createdAt: { $gt: new Date(req.body.startDate), $lt: new Date(req.body.endDate) } }],
+        createdAt: { $gte: new Date(body.startDate), $lte: new Date(body.endDate) },
       };
-
+      const countQuery = { totalCount: { $gte: body.minCount, $lte: body.maxCount } };
       const collection = Application.databaseManager.database.collection('records');
       const limit = req.query.limit ? parseInt(req.query.limit.toString()) : 100;
-      const [results, itemCount] = await Promise.all([
-        collection.aggregate([
-          {
-            $match: dateQuery,
+      const results = await collection.aggregate([
+        {
+          $match: dateQuery,
+        },
+        { $sort: { createdAt: -1 } },
+        {
+          $unwind: '$counts',
+        },
+        {
+          $group: {
+            _id: '$_id',
+            key: { $first: '$_id' },
+            createdAt: { $first: '$createdAt' },
+            totalCount: { $sum: '$counts' },
           },
-          { $sort: { createdAt: -1 } },
-          { $skip: req.skip ?? 0 },
-          { $limit: limit },
-          {
-            $unwind: '$counts',
+        },
+        {
+          $match: countQuery,
+        },
+        {
+          $project: {
+            _id: 0,
+            key: 1,
+            createdAt: 1,
+            totalCount: 1,
           },
-          {
-            $group: {
-              _id: '$_id',
-              key: { $first: '$_id' },
-              createdAt: { $first: '$createdAt' },
-              totalCount: { $sum: '$counts' },
-            },
+        },
+        {
+          $facet: {
+            paginatedResults: [{ $skip: req.skip ?? 0 }, { $limit: limit }],
+            totalCount: [
+              {
+                $count: 'count',
+              },
+            ],
           },
-          {
-            $project: {
-              _id: 0,
-              key: 1,
-              createdAt: 1,
-              totalCount: 1,
-            },
-          },
-        ]),
-        collection.count({}),
+        },
       ]);
-
-      const pageCount = Math.ceil(itemCount / limit);
       const array = await results.toArray();
-
+      const itemCount = array && array[0] && array[0].totalCount.lenght ? array[0].totalCount[0].count : 0;
+      const pageCount = Math.ceil(itemCount / limit);
       const pagination = {
         page: req.query.page ?? 1,
         per_page: limit,
         page_count: pageCount,
-        total_count: itemCount,
       };
 
-      return resolve({ records: array as Record[], pagination: pagination });
+      return resolve({ records: array[0].paginatedResults as Record[], pagination: pagination });
     });
   }
 }
